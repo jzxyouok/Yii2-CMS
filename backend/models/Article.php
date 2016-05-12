9cb23ef94b2780e7b6edcdbea505bdad8155aefe
LonelyDreamers <986083741@qq.com>
2016-05-12 17:54:33 +0800

    文章管理搜索模型

---
+++ backend/models/Article.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Article as ArticleModel;
+
+/**
+ * Article represents the model behind the search form about `common\models\Article`.
+ */
+class Article extends ArticleModel
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'category_id', 'created_at', 'hits', 'sort', 'status'], 'integer'],
+            [['title', 'sub_title', 'author', 'source', 'image', 'summary', 'content'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = ArticleModel::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'created_at' => $this->created_at,
+            'hits' => $this->hits,
+            'sort' => $this->sort,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'sub_title', $this->sub_title])
+            ->andFilterWhere(['like', 'author', $this->author])
+            ->andFilterWhere(['like', 'source', $this->source])
+            ->andFilterWhere(['like', 'image', $this->image])
+            ->andFilterWhere(['like', 'summary', $this->summary])
+            ->andFilterWhere(['like', 'content', $this->content]);
+
+        return $dataProvider;
+    }
+}
