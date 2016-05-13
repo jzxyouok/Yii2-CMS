3b72390b9427f31b72863333af6bd28ccc161930
LonelyDreamers <986083741@qq.com>
2016-05-13 17:40:53 +0800

    修改友链视图

--- backend/views/links/create.php
+++ backend/views/links/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Links */
 
-$this->title = '添加友情链接';
+$this->title = '添加友链';
 $this->params['breadcrumbs'][] = ['label' => '友情链接管理', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
