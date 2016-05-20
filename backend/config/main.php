ac211a292dd98a58d36a73e74c71b666522444b9
LonelyDreamers <986083741@qq.com>
2016-05-20 12:29:16 +0800

    表结构sql

--- backend/config/main.php
+++ backend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-backend',
+    'name' => '后台管理系统',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
